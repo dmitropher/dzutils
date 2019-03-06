37f9130d369ec95538ff4df7fd696097c19ae09f
dzorine <dzorine@ibex.dhcp.baker>
2019-03-06 15:59:55 -0800

    safety

--- pyrosetta_utils/superposition_utilities.py
+++ pyrosetta_utils/superposition_utilities.py
@@ -18,13 +18,15 @@ def superposition_pose(mob_pose, init_coords, ref_coords):
     return mob_pose
 
 
-def super_resi_by_atom_name(mob_pose, targ_pose, mob_index, targ_index, *args):
+def super_by_residues(mob_pose, targ_pose, mob_index, targ_index, *args):
     """
     superimposes mob_pose onto targ_pose: aligns by atom name from indexes given
 
     May provide any number of atom names as long as both are in each residue
     Offset can be provided to superimpose atoms of the same name with an offset
     (Useful for a lazy C symmetric rotation)
+
+    Defaults to superimposing by bb atoms if no atoms given
     """
     init_coords = (
         _pyrosetta.rosetta.utility.vector1_numeric_xyzVector_double_t()
@@ -34,11 +36,24 @@ def super_resi_by_atom_name(mob_pose, targ_pose, mob_index, targ_index, *args):
     )
     mob_res = mob_pose.residue(mob_index)
     targ_res = targ_pose.residue(targ_index)
-
-    index_range = range(len(args))
+    num_atoms = len(args)
+    if num_atoms == 0:
+        return super_resi_by_bb
+    index_range = range(num_atoms)
 
     for index in index_range:
         init_coords.append(mob_res.xyz(args[index]))
         ref_coords.append(targ_res.xyz(args[index]))
     superposition_pose(mob_pose, init_coords, ref_coords)
     return mob_pose
+
+
+def super_resi_by_bb(mob_pose, targ_pose, mob_index, targ_index):
+    init_coords = (
+        _pyrosetta.rosetta.utility.vector1_numeric_xyzVector_double_t()
+    )
+    ref_coords = (
+        _pyrosetta.rosetta.utility.vector1_numeric_xyzVector_double_t()
+    )
+
+    return
