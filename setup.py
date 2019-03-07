52a46fbdde233c9f523daa4da4e58213edc65d78
dzorine <dzorine@ibex.dhcp.baker>
2019-03-06 17:02:31 -0800

    fixing setup.py

--- setup.py
+++ setup.py
@@ -9,5 +9,9 @@ setup(
     description="Dmitri Zorine's utility scripts from the Baker Lab",
     author="Dmitri Zorine",
     author_email="dzorine@gmail.com",
-    packages=["dzutils", "dzutils/pyrosetta_utils", "dzutils/phos_binding"],
+    packages=[
+        "dzutils",
+        "dzutils/pyrosetta_utils",
+        "dzutils/pyrosetta_utils/phos_binding",
+    ],
 )
