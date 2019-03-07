59a6855f463ca7b5f3e7f7b104c63820a6f7a502
dzorine <dzorine@ibex.dhcp.baker>
2019-03-07 15:15:06 -0800

    safety

--- rt_utils.py
+++ rt_utils.py
@@ -1,13 +1,13 @@
 import pyrosetta as _pyrosetta
 
 
-def rt_from_res_atoms (start_res, end_res, start_center_atom, end_center_atom):
+def rt_from_res_atoms(start_res, end_res, start_center_atom, end_center_atom):
     """
     Returns the RT from the start to end res: centers stub around given atoms
     """
 
 
-def c_to_n_rt (start_res, end_res):
+def c_to_n_rt(start_res, end_res):
     """
     Returns the RT from the C term of start_res to the N of end_res
     """
