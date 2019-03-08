a9ef9aa4e1715f69ccc26f1a6216b5b7abde4bf8
dzorine <dzorine@ibex.dhcp.baker>
2019-03-08 10:45:15 -0800

    Added a file and some function skeletons

---
+++ dzutils/pyrosetta_utils/test_tools/test_poses.py
@@ -0,0 +1,28 @@
+import pyrosetta as _pyrosetta
+
+
+def rossmann_monomer():
+    """
+    returns a pose of pdb structure 2kpo
+
+    Loads from a pdb file, this is deliberately designed to be brittle to detect
+    when pyrosetta behavior has changed.
+    """
+
+
+def helical_monomer():
+    """
+    returns a pose of pdb structure 4uos
+
+    Loads from a pdb file, this is deliberately designed to be brittle to detect
+    when pyrosetta behavior has changed.
+    """
+
+
+def helical_trimer():
+    """
+    returns a pose of pdb structure
+    
+    Loads from a pdb file, this is deliberately designed to be brittle to detect
+    when pyrosetta behavior has changed.
+    """
