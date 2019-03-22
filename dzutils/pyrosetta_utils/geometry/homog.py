b0e957236b3bd96d588cdfc8980a81dd1a40e06f
dzorine <dzorine@gmail.com>
2019-03-22 10:33:32 -0700

    added some utils to homog

--- dzutils/pyrosetta_utils/geometry/homog.py
+++ dzutils/pyrosetta_utils/geometry/homog.py
@@ -1,1 +1,60 @@
 import numpy as _numpy
+from dzutils.pyrosetta_utils.geometry.rt_utils import (
+    stub_from_residue as _stub_from_residue,
+)
+
+
+def rotation_translation_to_homog(rotation, translation):
+    """
+    Takes a rotation matrix and a translation vector and returns a h xform
+    """
+    return numpy.array(
+        [
+            [rotation.xx, rotation.xy, rotation.xz, translation.x],
+            [rotation.yx, rotation.yy, rotation.yz, translation.y],
+            [rotation.zx, rotation.zy, rotation.zz, translation.z],
+            [0, 0, 0, 1],
+        ]
+    )
+
+
+def stub_to_homog(stub):
+    """
+    Takes a rosetta stube and returns a h xform
+    """
+    return rotation_translation_to_homog(stub.M, stub.v)
+
+
+def rt_to_homog(rt):
+    """
+    Takes a rosetta RT and returns a h xform
+    """
+    return rotation_translation_to_homog(
+        rt.get_rotation(), rt.get_translation()
+    )
+
+
+def invert_homog(xform):
+    """
+    Takes a homogenous xform and returns its inverse
+    """
+    inv = _numpy.linalg.inv(xform)
+    return inv
+
+
+def homog_relative_transform_from_stubs(stub1, stub2):
+    """
+    Takes two Rosetta stubs and converts them to a homogenous relative transform between the two
+    """
+    hstub1, hstub2 = stub_to_homog(stub1), stub_to_homog(stub2)
+    hstub1_inv = invert_homog(hstub1)
+    return hstub1_inv @ hstub2
+
+
+def homog_relative_transform_from_residues(res1, res2):
+    """
+    Wrapper for making CA to CA homogenouse relative transform between residues
+    """
+    return homog_relative_transform_from_stubs(
+        _stub_from_residue(res1), _stub_from_residue(res2)
+    )
