1da94a550cc8468216139425cd04e252f529bf66
dzorine <dzorine@gmail.com>
2019-03-26 13:55:48 -0700

    dug pose-xforms from backup

--- dzutils/pyrosetta_utils/geometry/__init__.py
+++ dzutils/pyrosetta_utils/geometry/__init__.py
@@ -1,4 +1,4 @@
 from . import rt_utils
 from . import superposition_utilities
 from . import homog
-from . import pose_stub_array
+from . import pose_xforms
---
+++ dzutils/pyrosetta_utils/geometry/pose_xforms.py
@@ -0,0 +1,122 @@
+import numpy as _np
+from dzutils.pyrosetta_utils.geometry.homog import (
+    stub_to_homog as _stub_to_homog,
+)
+from dzutils.pyrosetta_utils.geometry.homog import (
+    homog_relative_transform as _stubs_to_rt,
+)
+from dzutils.pyrosetta_utils.geometry.rt_utils import (
+    stub_from_residue as _stub_from_residue,
+)
+
+
+class PoseRTArray(_np.ndarray):
+    """
+    A class designed to wrap a numpy array with pose and residue options
+
+    specifically used to save the relative transform between two residues,
+    keeping track of the important atoms.
+
+    Instantiating this object with just stub xforms overrides other behavior,
+    otherwise the stubs are computed from the input pose and seqpos
+    """
+
+    def __new__(
+        cls,
+        pose_start=None,
+        pose_end=None,
+        seqpos_start=None,
+        seqpos_end=None,
+        stub_xform1=None,
+        stub_xform2=None,
+        atoms_start=("CA", "N", "CA", "C"),
+        atoms_end=("CA", "N", "CA", "C"),
+    ):
+        if type(stub_xform1) != None and type(stub_xform2) != None:
+            obj = _np.asarray(_stubs_to_rt(stub_xform1, stub_xform2)).view(cls)
+        else:
+            stub_xform1 = _stub_to_homog(
+                _stub_from_residue(
+                    pose_start.residue(seqpos_start), *atoms_start
+                )
+            )
+            stub_xform2 = _stub_to_homog(
+                _stub_from_residue(pose_end.residue(seqpos_start), *atoms_end)
+            )
+            obj = _np.asarray(_stubs_to_rt(stub_xform1, stub_xform2)).view(cls)
+        obj.pose_start = pose_start
+        obj.pose_end = pose_end
+        obj.seqpos_start = seqpos_start
+        obj.seqpos_end = seqpos_end
+        obj._atoms_start = atoms_start
+        obj._atoms_end = atoms_end
+        return obj
+
+    def __array_finalize__(self, obj):
+        if obj is None:
+            return
+        self.pose_start = getattr(obj, "pose_start", None)
+        self.pose_end = getattr(obj, "pose_end", None)
+        self.seqpos_start = getattr(obj, "seqpos_start", None)
+        self.seqpos_end = getattr(obj, "seqpos_end", None)
+        self._atoms_start = getattr(obj, "atoms_start", None)
+        self._atoms_end = getattr(obj, "atoms_end", None)
+
+
+class PoseStubArray(_np.ndarray):
+    """
+    A class designed to wrap a numpy array with pose and residue options
+    """
+
+    def __new__(cls, pose=None, seqpos=None, atoms=("CA", "N", "CA", "C")):
+        obj = _np.asarray(
+            _stub_to_homog(_stub_from_residue(pose.residue(seqpos), *atoms))
+        ).view(cls)
+        obj.pose = pose
+        obj.seqpos = seqpos
+        obj._atoms = atoms
+        return obj
+
+    def __array_finalize__(self, obj):
+        if obj is None:
+            return
+        self.pose = getattr(obj, "pose", None)
+        self.seqpos = getattr(obj, "seqpos", None)
+        self._atoms = getattr(obj, "atoms", None)
+
+    def set_atoms(self, atoms):
+        """
+        Takes a tuple of atoms and makes them the atoms for this object.
+
+        Recomputes the h transform
+        """
+        self._atoms = atoms
+        _np.place(
+            self,
+            _np.ones_like(self),
+            _np.asarray(
+                _stub_to_homog(
+                    _stub_from_residue(self.pose.residue(self.seqpos), *atoms)
+                )
+            ),
+        )
+
+    def get_rt_to_stub(self, stub):
+        """
+        Returns a wrapper for a homogxform ndarray describing the rt to the stub
+
+        Creates a local rotation translation homogeneous transform
+        returns a wrapper object that saves the pose, seqpos, and atoms that the
+        rt was generated from.
+        """
+        rt_array = PoseRTArray(
+            pose_start=self.pose,
+            pose_end=stub.pose,
+            seqpos_start=self.seqpos,
+            seqpos_end=stub.seqpos,
+            stub_xform1=self,
+            stub_xform2=stub,
+            atoms_start=self._atoms,
+            atoms_end=stub._atoms,
+        )
+        return rt_array
