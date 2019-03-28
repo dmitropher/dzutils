9a24f409a413554d981e32b7016b111f7b84ddd4
dzorine <dzorine@ibex.dhcp.baker>
2019-03-28 15:34:01 -0700

    added posenum in chain test

--- dzutils/pyrosetta_utils/test_chain_utils.py
+++ dzutils/pyrosetta_utils/test_chain_utils.py
@@ -1,4 +1,5 @@
 import unittest
+from unittest.mock import Mock as Mock
 import dzutils.pyrosetta_utils.chain_utils as chain_utils
 
 
@@ -6,3 +7,8 @@ class TestChainUtils(unittest.TestCase):
     """
     Test for chain_utils
     """
+
+    def test_posenum_in_chain():
+        pose = Mock()
+        pose.chain_begin = Mock(return_value=10)
+        assertEqual(1, chain_utils.posenum_in_chain(pose, 10))
