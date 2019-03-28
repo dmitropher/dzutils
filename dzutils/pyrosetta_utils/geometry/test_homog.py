7bc8702e3df0d085f11b6ade7bc8354315144474
dzorine <dzorine@gmail.com>
2019-03-28 14:03:24 -0700

    working on test

--- dzutils/pyrosetta_utils/geometry/test_homog.py
+++ dzutils/pyrosetta_utils/geometry/test_homog.py
@@ -1,6 +1,7 @@
 import homog
 import unittest
 import numpy as _np
+import test_poses as _tp
 
 
 class TestCreation(unittest.TestCase):
@@ -11,10 +12,10 @@ class TestCreation(unittest.TestCase):
     def test_rotation_translation_to_homog(self):
         rosetta_matrix = Mock()
         rosetta_vector = Mock()
-        rosetta_matrix.xx, rosetta_matrix.xy, rosetta_matrix.xz = 0, 1, 1
-        rosetta_matrix.yx, rosetta_matrix.yy, rosetta_matrix.yz = 1, 1, 2
-        rosetta_matrix.zx, rosetta_matrix.zy, rosetta_matrix.zz = 0, 2, 1
-        rosetta_vector.x, rosetta_vector.y, rosetta_vector.z = 3, 2, 1
+        rosetta_matrix1.xx, rosetta_matrix1.xy, rosetta_matrix1.xz = 0, 1, 1
+        rosetta_matrix1.yx, rosetta_matrix1.yy, rosetta_matrix1.yz = 1, 1, 2
+        rosetta_matrix1.zx, rosetta_matrix1.zy, rosetta_matrix1.zz = 0, 2, 1
+        rosetta_vector1.x, rosetta_vector1.y, rosetta_vector1.z = 3, 2, 1
 
         hardcode = _np.array(
             [[0, 1, 1, 3], [1, 1, 2, 2], [0, 2, 1, 1], [0, 0, 0, 1]]
@@ -27,10 +28,10 @@ class TestCreation(unittest.TestCase):
     def test_stub_to_homog(self):
         rosetta_matrix = Mock()
         rosetta_vector = Mock()
-        rosetta_matrix.xx, rosetta_matrix.xy, rosetta_matrix.xz = 0, 1, 1
-        rosetta_matrix.yx, rosetta_matrix.yy, rosetta_matrix.yz = 1, 1, 2
-        rosetta_matrix.zx, rosetta_matrix.zy, rosetta_matrix.zz = 0, 2, 1
-        rosetta_vector.x, rosetta_vector.y, rosetta_vector.z = 3, 2, 1
+        rosetta_matrix1.xx, rosetta_matrix1.xy, rosetta_matrix1.xz = 0, 1, 1
+        rosetta_matrix1.yx, rosetta_matrix1.yy, rosetta_matrix1.yz = 1, 1, 2
+        rosetta_matrix1.zx, rosetta_matrix1.zy, rosetta_matrix1.zz = 0, 2, 1
+        rosetta_vector1.x, rosetta_vector1.y, rosetta_vector1.z = 3, 2, 1
         stub = Mock()
         stub.M = rosetta_matrix
         stub.v = rosetta_vector
@@ -43,10 +44,10 @@ class TestCreation(unittest.TestCase):
     def test_rt_to_homog(self):
         rosetta_matrix = Mock()
         rosetta_vector = Mock()
-        rosetta_matrix.xx, rosetta_matrix.xy, rosetta_matrix.xz = 0, 1, 1
-        rosetta_matrix.yx, rosetta_matrix.yy, rosetta_matrix.yz = 1, 1, 2
-        rosetta_matrix.zx, rosetta_matrix.zy, rosetta_matrix.zz = 0, 2, 1
-        rosetta_vector.x, rosetta_vector.y, rosetta_vector.z = 3, 2, 1
+        rosetta_matrix1.xx, rosetta_matrix1.xy, rosetta_matrix1.xz = 0, 1, 1
+        rosetta_matrix1.yx, rosetta_matrix1.yy, rosetta_matrix1.yz = 1, 1, 2
+        rosetta_matrix1.zx, rosetta_matrix1.zy, rosetta_matrix1.zz = 0, 2, 1
+        rosetta_vector1.x, rosetta_vector1.y, rosetta_vector1.z = 3, 2, 1
         rt = Mock()
         rt.get_rotation = rosetta_matrix
         rt.get_translation = rosetta_vector
@@ -76,4 +77,37 @@ class TestCreation(unittest.TestCase):
         assertEqual(
             inv @ hardcode2,
             homog.homog_relative_transform(hardcode1, hardcode2),
+        )
+
+    def test_homog_relative_transform_from_stubs(self):
+
+        stub1, stub2 = Mock(), Mock()
+        rosetta_matrix1, rosetta_matrix2 = Mock(), Mock()
+        rosetta_vector1, rosetta_vector2 = Mock(), Mock()
+        rosetta_matrix1.xx, rosetta_matrix1.xy, rosetta_matrix1.xz = 0, 1, 1
+        rosetta_matrix1.yx, rosetta_matrix1.yy, rosetta_matrix1.yz = 1, 1, 2
+        rosetta_matrix1.zx, rosetta_matrix1.zy, rosetta_matrix1.zz = 0, 2, 1
+        rosetta_vector1.x, rosetta_vector1.y, rosetta_vector1.z = 3, 2, 1
+        rosetta_matrix2.xx, rosetta_matrix2.xy, rosetta_matrix2.xz = 0, 1, 1
+        rosetta_matrix2.yx, rosetta_matrix2.yy, rosetta_matrix2.yz = 1, 1, 2
+        rosetta_matrix2.zx, rosetta_matrix2.zy, rosetta_matrix2.zz = 0, 2, 1
+        rosetta_vector2.x, rosetta_vector2.y, rosetta_vector2.z = 3, 2, 1
+        stub1.M = rosetta_matrix1
+        stub1.v = rosetta_vector1
+        stub2.M = rosetta_matrix2
+        stub2.v = rosetta_vector2
+        ident = _np.identity(4)
+        assertEqual(
+            ident, homog.homog_relative_transform_from_stubs(stub1, stub2)
+        )
+
+    def test_homog_relative_transform_from_residues(self):
+        import rt_utils as _rtu
+
+        pose = _tp.helical_monomer
+        stub1 = _rtu.stub_from_residue(pose.residue(1))
+        stub2 = _rtu.stub_from_residue(pose.residue(1))
+        ident = _np.identity(4)
+        self.assertEqual(
+            ident, homog.homog_relative_transform_from_residues(stub1, stub2)
         )
