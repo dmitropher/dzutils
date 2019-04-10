e169b2574c6a08a2de649ed4b2257e1a284b56fa
dzorine <dzorine@ibex.dhcp.baker>
2019-04-09 17:44:53 -0700

    added the circle generation stuff

--- dzutils/pyrosetta_utils/geometry/chi_atom_circle.py
+++ dzutils/pyrosetta_utils/geometry/chi_atom_circle.py
@@ -1,7 +1,6 @@
-import pyrosetta as _pyrosetta
 import numpy as _np
 from dzutils.pyrosetta_utils.geometry.homog import (
-    rotation_translation_to_homog as _rt_to_homog,
+    np_rot_trans_to_homog as _to_homog,
 )
 
 # Do for every dock
@@ -29,20 +28,20 @@ class ChiAtomCircle:
         self._chi_atoms = [
             a for a in self._residue.type().chi_atoms()[self._chi]
         ]
-        self._atom_xyz = self._resiude.xyz(self._atom)
+        self._atom_xyz = self._residue.xyz(atom)
         self._atom_num = (
-            self._atom
-            if int(atom) == atom
+            atom
+            if isinstance(atom, int)
             else [
                 i
-                for i, atom in enumerate(self._residue.atoms(), 1)
-                if atom.mm_type() == self._residue.atom(atom).mm_type()
+                for i, a in enumerate(self._residue.atoms(), 1)
+                if a.mm_type() == self._residue.atom(atom).mm_type()
             ][0]
         )
         self._atom_name = (
-            atom if str(atom) == atom else self._residue.atom_name(atom)
+            atom if isinstance(atom, str) else self._residue.atom_name(atom)
         )
-        self._atom = self._residue(atom)
+        self._atom = self._residue.atom(atom)
 
     def compute_circle(self):
         """
@@ -55,23 +54,33 @@ class ChiAtomCircle:
         unit vector down the chi in either the for or rev rotamer direction.
         """
         # Figure out whether to compute inverse or forward rotamer circle
-        vec_atoms = (
-            (self._chi_atoms[1], self._chi_atoms[2])
-            if self._atom_num < self._chi_atoms[1]
-            else (self._chi_atoms[2], self._chi_atoms[1])
+        self._inverse_rotamer = (
+            True if self._atom_num < self._chi_atoms[1] else False
         )
-        chi_origin = np.array([coor for coor in vec_atoms[0].xyz()])
-        atom_xyz = np.array([coor for coor in self._atom.xyz()])
-        chi_vec = np.array([coor for coor in vec_atoms[1].xyz()]) - chi_origin
-        unit_chi_vec = chi_vec / np.linalg.norm(chi_vec)
+        vec_atoms = (
+            (self._chi_atoms[2], self._chi_atoms[1])
+            if self._inverse_rotamer
+            else (self._chi_atoms[1], self._chi_atoms[2])
+        )
+
+        chi_origin = _np.array(
+            [coor for coor in self._residue.xyz(vec_atoms[0])]
+        )
+        atom_xyz = _np.array([coor for coor in self._atom.xyz()])
+        chi_vec = (
+            _np.array([coor for coor in self._residue.xyz(vec_atoms[1])])
+            - chi_origin
+        )
+        unit_chi_vec = chi_vec / _np.linalg.norm(chi_vec)
         hyp_vec = atom_xyz - chi_origin
-        hyp_norm = np.linalg.norm(hyp_vec)
+        hyp_norm = _np.linalg.norm(hyp_vec)
         unit_hyp_vec = hyp_vec / hyp_norm
-        cos_theta = self._normal_vector * unit_hyp_vec
+        cos_theta = unit_chi_vec * unit_hyp_vec
         self._normal_vector = unit_chi_vec
-        self._circle_origin = chi_origin + unit_chi_vec * hyp_norm
-        self._circle_radius = np.linalg.norm(
-            aotm_xyz - self._circle_origin - atom_xyz
+        self._circle_origin = chi_origin + unit_chi_vec * cos_theta
+        self._circle_radius = (
+            _np.linalg.norm(_np.cross(self._normal_vector, unit_hyp_vec))
+            * hyp_norm
         )
 
     def compute_rt_to_xy(self):
@@ -85,13 +94,17 @@ class ChiAtomCircle:
             translation = self._circle_origin
             # if the circle normal is the negative of the xy normal,
             # then the other equation doesn't work, this hardcode works fine tho
-            if np.array_equal(self._normal_vector, np.linalg([[0][0][-1]])):
-                rotation = np.array([[-1, 0, 0], [0, 1, 0], [0, 0, -1]])
-                return _rt_to_homog(rotation, translation)
+            if _np.array_equal(self._normal_vector, _np.array([0, 0, -1])):
+                rotation = _np.array([[-1, 0, 0], [0, 1, 0], [0, 0, -1]])
+                self._rt_to_xy = _rt_to_homog(
+                    rotation, _np.transpose(translation)
+                )
+                self._rt_to_xy = _to_homog(rotation, translation)
+                return self._rt_to_xy
             else:
-                xy_unit_normal = np.array([[0], [0], [1]])
-                ortho = np.cross(self._normal_vector)
-                ortho_x = np.array(
+                xy_unit_normal = _np.array([0, 0, 1])
+                ortho = _np.cross(self._normal_vector, xy_unit_normal)
+                ortho_x = _np.array(
                     [
                         [0, -ortho[2], ortho[1]],
                         [ortho[2], 0, -ortho[0]],
@@ -99,16 +112,17 @@ class ChiAtomCircle:
                     ]
                 )
                 rotation = (
-                    np.identity(3)
+                    _np.identity(3)
                     + ortho_x
                     + ortho_x
                     @ ortho_x
                     * (1 / (1 - (self._normal_vector * xy_unit_normal)))
                 )
-                return _rt_to_homog(rotation, translation)
+                self._rt_to_xy = _to_homog(rotation, translation)
+                return self._rt_to_xy
         except AttributeError as ae:
             self.compute_circle()
-            return self.compute_rt_to_xy()
+            self.compute_rt_to_xy()
 
     def get_radius(self):
         """
@@ -122,7 +136,7 @@ class ChiAtomCircle:
             self.compute_circle()
             return self._circle_radius
 
-    def get_circle_origin():
+    def get_circle_origin(self):
         """
         Returns the origin of the circle the atom can be in
 
@@ -134,7 +148,7 @@ class ChiAtomCircle:
             self.compute_circle()
             return self._circle_origin
 
-    def get_normal_vector():
+    def get_normal_vector(self):
         """
         Returns the normal vector of the circle the atom can be in
 
@@ -146,7 +160,7 @@ class ChiAtomCircle:
             self.compute_circle()
             return self._normal_vector
 
-    def get_rt_to_xy_plane():
+    def get_rt_to_xy_plane(self):
         """
         Returns the RT from orgin/normal vector to 0,0,0/0,0,1 (the xy plane)
 
@@ -156,12 +170,4 @@ class ChiAtomCircle:
             return self._rt_to_xy
         except AttributeError as no_rad:
             self.compute_rt_to_xy()
-            return self._rt_to_xz()
-
-
-# Do for every CA
-# compute projection onto circle in 3D
-# move projection to origin
-# get the equation of the line in Will-space ([[0,x_proj/y_proj,0],[-1,0,0],[0,0,0]])
-# Should give two roots, just take the root that's closer to (x_proj,y_proj)
-# apply the inverse of rotation translation to origin
+            return self._rt_to_xy()
