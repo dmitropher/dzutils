746f7f075e703d6ac3e1888df28565b00865999f
dzorine <dzorine@gmail.com>
2019-04-23 11:22:08 -0700

    um, commit?

--- dzutils/func_utils.py
+++ dzutils/func_utils.py
@@ -10,3 +10,24 @@ def compose(*functions):
         return lambda x: f(g(x))
 
     return _functools.reduce(compose2, functions, lambda x: x)
+
+def boil_fuction (func,*args,**kwargs):
+    """
+    Returns the output of func(*args,**kwargs), on exception returns None
+
+    Jenky utility for working with ambiguous inputs
+    """
+    try:
+        return func(*args, **kwargs)
+    except:
+        return None
+def boil_generator (func,*args,**kwargs):
+    """
+    Returns the output of func(*args,**kwargs), on exception returns empty iterable
+
+    Jenky utility for working with ambiguous inputs
+    """
+    try:
+        return func(*args,**kwargs)
+    except:
+        return iter(())
