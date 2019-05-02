45691cfc7f8cd815e9014b603aa3c38dd0f9697c
dmitropher <dzorine@gmail.com>
2019-05-02 13:12:11 -0700

    cleaning up jupyter, added some misc stuff

--- dzutils/pyrosetta_utils/util.py
+++ dzutils/pyrosetta_utils/util.py
@@ -21,3 +21,39 @@ def residues_by_name(pose, *names, include_res=False):
             for i, is_res in enumerate(pose.residues, 1)
             if is_res.name3() in names
         ]
+
+
+def atom_distance(a1, a2):
+    """
+    Takes rosetta atoms, returns the norm of a1 - a2
+    """
+    try:
+        return (a1.xyz() - a2.xyz()).norm()
+    except:
+        return None
+
+
+def index_neighbors(pose, index, dist):
+    """
+    Returns a list of indexes that are neighbors to the given index
+    """
+    return [
+        resi
+        for resi, is_neib in enumerate(
+            _pyr.core.select.residue_selector.NeighborhoodResidueSelector(
+                _pyr.core.select.residue_selector.ResidueIndexSelector(
+                    str(index)
+                ),
+                dist,
+            ).apply(pose),
+            1,
+        )
+        if is_neib
+    ]
+
+
+def get_index_atom(pose, index, atom):
+    try:
+        return pose.residue(index).atom(atom)
+    except:
+        return None
