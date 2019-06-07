3b08910fcf314cdc132932799ddd2d049e221b8a
dzorine <dzorine@ibex.dhcp.baker>
2019-06-07 14:02:39 -0700

    moved hbond func to pyrosetta_utils utils

--- dzutils/pyrosetta_utils/util.py
+++ dzutils/pyrosetta_utils/util.py
@@ -121,6 +121,25 @@ def exclude_by_label_residue_selector(label):
     return notSelector
 
 
+def hbond_to_residue(pose, resnum, vec=False):
+    """
+    Returns a list of all the hbonding residues to the given residue
+
+    vec=True returns a rosetta vector instead (marginally faster)
+    """
+    if vec:
+        return _pyr.core.scoring.hbonds.HBondSet(
+            pose, bb_only=False
+        ).residue_hbonds(resnum)
+    else:
+        return [
+            b
+            for b in _pyr.core.scoring.hbonds.HBondSet(
+                pose, bb_only=False
+            ).residue_hbonds(resnum)
+        ]
+
+
 def residues_with_element(pose, *elements):
     """
     Returns a list of resnums where element is in some atom of the residue
