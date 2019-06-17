87f6aafa9ece4fdda808b6fc0d89a37e13f76192
dzorine <dzorine@ibex.dhcp.baker>
2019-06-17 13:57:27 -0700

    beginning initial testing of run script

--- dzutils/pyrosetta_utils/phos_binding/double_scan_pbinders.py
+++ dzutils/pyrosetta_utils/phos_binding/double_scan_pbinders.py
@@ -3,8 +3,8 @@
 # Hunt for inverse rotamers to complete interaction
 
 # Takes: pdb, out_dir, out_prefix (uses defaults)
-
-import glob
+import sys
+from os.path import basename
 
 import pandas as pd
 import getpy as gp
@@ -84,46 +84,46 @@ for col in loop_table:
     results[f"loop_{col}"] = results["e2e_inds"].map(
         lambda index: loop_table[loop_table["index"] == index][col].item()
     )
-# results.loc[:, "file"] = results["e2e_inds"].map(
-#     lambda index: loop_table[loop_table["index"] == index]["file"].item()
-# )
-# results.loc[:, "key"] = results["e2e_inds"].map(
-#     lambda index: loop_table[loop_table["index"] == index]["key_int"].item()
-# )
-# results.loc[:, "func_to_bb_start"] = results["e2e_inds"].map(
-#     lambda index: loop_table[loop_table["index"] == index]["func_to_bb_start"].item()
-# )
-# results.loc[:, "e2e"] = results["e2e_inds"].map(
-#     lambda index: loop_table[loop_table["index"] == index]["e2e"].item()
-# )
 
 not_expanded_allowed_res = results
-results = results.allowed_res.apply(pd.Series).merge(results,right_index=True,left_index=True)\
-    .melt(id_vars = [*results],value_names="p_res_target")\
-    .drop(["allowed_res","variable"],axis = 1)
+results = (
+    results.allowed_res.apply(pd.Series)
+    .merge(results, right_index=True, left_index=True)
+    .melt(id_vars=[*results], value_names="p_res_target")
+    .drop(["allowed_res", "variable"], axis=1)
+)
 
 results["inv_rot_key"] = results.apply(
-    lambda row:
-            binner.get_bin_index(
-                row["func_to_bb_start"]
-                @ generate_pose_rt_between_res(pose, row["start_res"], row["p_res_target"])
-            )
-,
+    lambda row: binner.get_bin_index(
+        row["func_to_bb_start"]
+        @ generate_pose_rt_between_res(
+            pose, row["start_res"], row["p_res_target"]
+        )
+    ),
     axis=1,
 )
-inv_rot_dict_path = "/home/dzorine/projects/phos_binding/pilot_runs/loop_grafting/fragment_tables/inverse_rotamer/dicts/inverse_ptr_exchi7_rotamers_1ang_v1.bin"
-inv_rot_table_path = "/home/dzorine/projects/phos_binding/pilot_runs/loop_grafting/fragment_tables/inverse_rotamer/tables/inverse_ptr_exchi7_rotamers_1ang_v1.json"
+
+inv_rot_data_path = "/home/dzorine/projects/phos_binding/pilot_runs/loop_grafting/fragment_tables/inverse_rotamer"
+inv_rot_data_name = "inverse_ptr_exchi7_rotamers_1ang_v1"
+inv_rot_dict_path = f"{inv_rot_data_path}/dicts/{inv_rot_data_name}.bin"
+inv_rot_table_path = f"{inv_rot_data_path}/tables/{inv_rot_data_name}.json"
 key_type, value_type = np.dtype("i8"), np.dtype("i8")
-inv_rot_table, inv_rot_dict = load_table_and_dict(inv_rot_table_path,inv_rot_dict_path,key_type, value_type,)
-
-
-results["res_has_key"] = results.apply(
-    lambda row:
-            inv_rot_dict.contains(np.array(row["inv_rot_key"])),
-    axis=1,
+inv_rot_table, inv_rot_dict = load_table_and_dict(
+    inv_rot_table_path, inv_rot_dict_path, key_type, value_type
 )
-# results["inv_rot_table_index"] = results.apply(
-#     lambda row: inv_rot_dict[np.array(row["inv_rot_key"]],
-#     axis=1,
-# )
-#output to file
+
+results["inv_rot_table_index"] = results.apply(
+    lambda row: inv_rot_dict[(np.array(row["inv_rot_key"]))], axis=1
+)
+
+for col in inv_rot_table:
+    results[f"inv_rot_{col}"] = results["index"].map(
+        lambda index: inv_rot_table[inv_rot_table["index"] == index][col].item()
+    )
+
+outpath = sys.argv[2]
+
+pdb_name = basename (pose.pdb_info().name()).split(".pdb")[-1]
+
+results.name = "double_ploop_hits"
+write_path = f"{outpath}/{results.name}_{pdb_name}.json"
