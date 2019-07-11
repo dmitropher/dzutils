91becd254232fd52fd3984e628c21555b7e3eec2
dmitropher <dzorine@gmail.com>
2019-07-11 11:22:22 -0700

    cleaned up make_rots

--- dzutils/pyrosetta_utils/phos_binding/misc_scripts/make_rots.py
+++ dzutils/pyrosetta_utils/phos_binding/misc_scripts/make_rots.py
@@ -1,7 +1,10 @@
 import pyrosetta
 import pyrosetta.rosetta as pyr
 
-import dzutils.pyrosetta_utils.geometry.superposition_utilities as su
+from dzutils.pyrosetta_utils.geometry.superposition_utilities import (
+    super_by_residues,
+)
+from dzutils.pyrosetta_utils import bonded_atoms, atom_indices_with_element
 
 pyrosetta.init(
     """-out:level 100
@@ -10,34 +13,89 @@ pyrosetta.init(
     -packing:ex2
     -packing:ex3
     -packing:ex4
-    -packing:ex1:level 7
-    -packing:ex2:level 7
-    -packing:ex3:level 7
-    -packing:ex4:level 7
 
 """
-    # -extra_res_fa /home/dzorine/phos_binding/p_compounds/residues/PTR.params
 )
-pose = pyrosetta.pose_from_sequence("YYY")
-mutate_res = pyrosetta.rosetta.protocols.simple_moves.MutateResidue()
-phy_index = 2
-mut_index = phy_index
-mut_name = "PTR"
-mutate_res.set_target(str(mut_index))
-mutate_res.set_res_name(mut_name)
-mutate_res.apply(pose)
-rt = pose.residue(2).type()
-print(rt)
-rots = [rot for rot in pyr.core.pack.rotamer_set.bb_independent_rotamers(rt)]
-out_pose = pyr.core.pose.Pose()
-rot_1 = rots[0]
-out_pose.append_residue_by_bond(rot_1)
-template = out_pose.clone()
-for i, rot in enumerate(rots[1:], 1):
-    temp = pyr.core.pose.Pose()
-    temp.append_residue_by_bond(rot)
-    su.super_by_residues(temp, template, 1, 1, *("O1P", "O2P", "P", "O3P"))
-    out_pose.append_residue_by_jump(temp.residue(1), i)
-out_pose.dump_pdb(
-    "/home/dzorine/phos_binding/pilot_runs/loop_grafting/initial_testing/toy_scan_test/ptr_rots_fix/ptr_rots.pdb"
-)
+# pyrosetta.init(
+#     """-out:level 100
+#     -extra_res_fa /home/dzorine/phos_binding/p_compounds/residues/PTR.params
+#     -packing:ex1
+#     -packing:ex2
+#     -packing:ex3
+#     -packing:ex4
+#     -packing:ex1:level 7
+#     -packing:ex2:level 7
+#     -packing:ex3:level 7
+#     -packing:ex4:level 7
+#
+# """
+#     # -extra_res_fa /home/dzorine/phos_binding/p_compounds/residues/PTR.params
+# )
+# pose = pyrosetta.pose_from_sequence("YYY")
+# mutate_res = pyrosetta.rosetta.protocols.simple_moves.MutateResidue()
+# phy_index = 2
+# mut_index = phy_index
+# mut_name = "PTR"
+# mutate_res.set_target(str(mut_index))
+# mutate_res.set_res_name(mut_name)
+# mutate_res.apply(pose)
+def one_res_pose(resname):
+
+    pose = pyr.core.pose.Pose()
+    res = pyr.core.conformation.Residue(
+        pyr.core.pose.get_restype_for_pose(pose, resname), True
+    )
+    pose.append_residue_by_bond(res)
+    return pose
+
+
+def rotamers_for_resname(resname):
+    """
+    Creates the backbone independent rotamer set for resname
+    """
+    return [
+        rot
+        for rot in pyr.core.pack.rotamer_set.bb_independent_rotamers(
+            one_res_pose(resname).residue(1).type()
+        )
+    ]
+
+
+def rot_poses(resname, *super_atoms):
+    """
+    returns a list of poses where each is a different rotamer of restype
+
+    leaving super_atoms blank gives forward rotamers, giving super_atoms
+    aligns all poses to those atoms
+    """
+    rots = rotamers_for_resname(resname)
+    pose = pyr.core.pose.Pose()
+    pose.append_residue_by_bond(rots[0])
+    poses = [pose]
+    for i, rot in enumerate(rots[1:]):
+        pose = pyr.core.pose.Pose()
+        pose.append_residue_by_bond(rot)
+        super_by_residues(pose, poses[i], 1, 1, *super_atoms)
+        poses.append(pose)
+    return poses
+
+
+def main():
+    p_res = pyr.core.conformation.Residue(
+        pyr.core.pose.get_restype_for_pose(pyr.core.pose.Pose(), "PTR"), True
+    )
+    p_atoms = [
+        "P",
+        *bonded_atoms(p_res, *atom_indices_with_element(p_res, "P")),
+    ]
+
+    poses = rot_poses("PTR", *p_atoms)
+    out_pose = poses[0]
+    for pose in poses[1:]:
+        pyr.core.pose.append_pose_to_pose(out_pose, pose, True)
+
+    out_pose.dump_pdb("/home/dzorine/temp/ptr_rots.pdb")
+
+
+if __name__ == "__main__":
+    main()
