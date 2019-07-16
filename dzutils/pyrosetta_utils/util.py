eef552d688b2778c33025c64c088a0f0831108e6
dzorine <dzorine@ibex.dhcp.baker>
2019-07-16 14:51:11 -0700

    working on refinement funcs

--- dzutils/pyrosetta_utils/util.py
+++ dzutils/pyrosetta_utils/util.py
@@ -224,6 +224,7 @@ def build_hbond_set(
     return hbond_set
 
 def hbond_to_residue(pose, resnum, hbond_set=None, vec=False):
+
     """
     Returns a list of all the hbonding residues to the given residue
 
@@ -231,6 +232,7 @@ def hbond_to_residue(pose, resnum, hbond_set=None, vec=False):
     """
     if not hbond_set:
         hbond_set = build_hbond_set(pose)
+
     if vec:
         return hbond_set.residue_hbonds(resnum)
     else:
