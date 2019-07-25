81ae11b1ec350f8e57e3a25a1df659b57a133521
dzorine <dzorine@gmail.com>
2019-07-25 12:01:22 -0700

    ready to test fragment picker

--- dzutils/pyrosetta_utils/phos_binding/misc_scripts/screen_pdb_for_ploops.py
+++ dzutils/pyrosetta_utils/phos_binding/misc_scripts/screen_pdb_for_ploops.py
@@ -152,43 +152,47 @@ def minimal_fragments_by_secondary_structure(
     return out_list
 
 
-ploop_flags_file = "/home/dzorine/phos_binding/pilot_runs/loop_grafting/initial_testing/misc_files/p_ligand.flags"
-run_pyrosetta_with_flags(ploop_flags_file)
-# get pose
-pose = pyrosetta.pose_from_pdb(sys.argv[1])
-# get_outdir
-outdir = sys.argv[2]
-name = pose.pdb_info().name().split("/")[-1].split(".pdb")[0]
-# Scan pose for phosphorus containing residues
-# extract hbonds to these residues where:
-# vec False to use list rather than rosetta vector
+def main ():
+    ploop_flags_file = "/home/dzorine/phos_binding/pilot_runs/loop_grafting/initial_testing/misc_files/p_ligand.flags"
+    run_pyrosetta_with_flags(ploop_flags_file)
+    # get pose
+    pose = pyrosetta.pose_from_pdb(sys.argv[1])
+    # get_outdir
+    outdir = sys.argv[2]
+    name = pose.pdb_info().name().split("/")[-1].split(".pdb")[0]
+    # Scan pose for phosphorus containing residues
+    # extract hbonds to these residues where:
+    # vec False to use list rather than rosetta vector
 
 
-# no support for different append/prepend values
-append_factor = 3  # +- 0-3 residues
+    # no support for different append/prepend values
+    append_factor = 3  # +- 0-3 residues
 
 
-num_contacts = int(sys.argv[3])
+    num_contacts = int(sys.argv[3])
 
-pose_size = len(pose.residues)
+    pose_size = len(pose.residues)
 
 
-# Extract contiguous loops with these contacts:
-#   - for each bb pair, check if intervening sequence is under 10 res
-#   - prepend and apppend +- 3 residues on each side if they exist
+    # Extract contiguous loops with these contacts:
+    #   - for each bb pair, check if intervening sequence is under 10 res
+    #   - prepend and apppend +- 3 residues on each side if they exist
 
-for d in minimal_fragments_by_contact_number(
-    pose, min_contacts=num_contacts, append_factor=append_factor
-):
+    for d in minimal_fragments_by_contact_number(
+        pose, min_contacts=num_contacts, append_factor=append_factor
+    ):
 
-    r = d["acceptor_res"]
-    s = d["start"]
-    e = d["end"]
-    p = link_poses(
-        pyrosetta.rosetta.protocols.grafting.return_region(pose.clone(), r, r),
-        pyrosetta.rosetta.protocols.grafting.return_region(pose, s, e),
-        rechain=True,
-    )
-    p.dump_pdb(
-        f"{outdir}/{name}_{num_contacts}-contacts_phos-{r}_frag_{s}-{e}.pdb"
-    )
+        r = d["acceptor_res"]
+        s = d["start"]
+        e = d["end"]
+        p = link_poses(
+            pyrosetta.rosetta.protocols.grafting.return_region(pose.clone(), r, r),
+            pyrosetta.rosetta.protocols.grafting.return_region(pose, s, e),
+            rechain=True,
+        )
+        p.dump_pdb(
+            f"{outdir}/{name}_{num_contacts}-contacts_phos-{r}_frag_{s}-{e}.pdb"
+        )
+
+if __name__ == "__main__":
+    main()
