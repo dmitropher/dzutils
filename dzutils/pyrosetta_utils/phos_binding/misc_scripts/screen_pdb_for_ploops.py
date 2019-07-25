a09f1310c5be4dd63f18fbf70f1f06240fbeb74c
dmitropher <dzorine@gmail.com>
2019-07-24 18:35:00 -0700

    nothing works yet commit

--- dzutils/pyrosetta_utils/phos_binding/misc_scripts/screen_pdb_for_ploops.py
+++ dzutils/pyrosetta_utils/phos_binding/misc_scripts/screen_pdb_for_ploops.py
@@ -65,11 +65,7 @@ def get_acceptor_res_for_hbond_collection(hbond_collection):
     return acceptor_res[0]
 
 
-
-
-def minimal_fragments_by_contact_number(
-    pose, min_contacts=1, append_factor=0
-):
+def minimal_fragments_by_contact_number(pose, min_contacts=1, append_factor=0):
     """
     Returns fragment dict with acceptor res and the span between donor residues
 
@@ -79,7 +75,7 @@ def minimal_fragments_by_contact_number(
 
     hbond_collection = get_bb_hbonds(pose)
 
-    pose_size = len(pose.residues )
+    pose_size = len(pose.residues)
 
     append_ranges = [range(append_factor + 1)] * 2
     return [
@@ -101,9 +97,39 @@ def minimal_fragments_by_contact_number(
     ]
 
 
-def minimal_fragments_by_secondary_structure (pose):
+def minimal_fragments_by_secondary_structure(
+    pose, *struct_types, min_contacts=1, proximity=5
+):
     """
+    returns fragments with adjacent secondary structure to contacts
+
+    Loops are not considered to be secondary structure
     """
+    bb_hbonds = get_bb_hbonds(pose)
+
+    struct_list = parse_structure_from_dssp(pose, *struct_types)
+    sec_struct_by_start_pos = {
+        struct.start_pos: struct for struct in struct_list
+    }
+    sec_struct_by_end_pos = {struct.end_pos: struct for struct in struct_list}
+    contacts = [
+        (r, min(*contact_set), max(*contact_set))
+        for hbonds in hbond_collection
+        for r in [get_acceptor_res_for_hbond_collection(hbonds)]
+        if len(hbonds) >= min_contacts
+        for contact_set in it.combinations(
+            [bond.don_res() for bond in hbonds], min_contacts
+        )
+        if max(*contact_set) - min(*contact_set) < 11
+        if min(*contact_set) - x > 1 and max(*contact_set) + y < pose_size
+    ]
+    for resnum, start_contact, end_contact in contacts:
+        end_structs = [
+            sec_struct_by_end_pos[i]
+            for i in range(end_contact, end_contact + proximity + 1)
+            if i in sec_struct_by_end_pos
+        ]
+
 
 ploop_flags_file = "/home/dzorine/phos_binding/pilot_runs/loop_grafting/initial_testing/misc_files/p_ligand.flags"
 run_pyrosetta_with_flags(ploop_flags_file)
@@ -131,9 +157,7 @@ pose_size = len(pose.residues)
 #   - prepend and apppend +- 3 residues on each side if they exist
 
 for d in minimal_fragments_by_contact_number(
-    pose,
-    min_contacts=num_contacts,
-    append_factor=append_factor,
+    pose, min_contacts=num_contacts, append_factor=append_factor
 ):
 
     r = d["acceptor_res"]
