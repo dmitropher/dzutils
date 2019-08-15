70970e6770b3c9beca79ca652fd0ec0f0efc3640
dzorine <dzorine@ibex.dhcp.baker>
2019-08-15 16:38:33 -0700

    ready for initial testing

--- dzutils/pyrosetta_utils/phos_binding/misc_scripts/scan_bundle_from_params.py
+++ dzutils/pyrosetta_utils/phos_binding/misc_scripts/scan_bundle_from_params.py
@@ -1,5 +1,7 @@
 import sys, json, os
 
+import click
+
 import pyrosetta
 
 from dzutils.pyrosetta_utils.phos_binding.parametric import (
@@ -17,6 +19,8 @@ from dzutils.pyrosetta_utils.phos_binding import (
     replace_res_from_pose,
 )
 from dzutils.pyrosetta_utils.chain_utils import chain_of
+
+from dzutils.pyrosetta_utils import run_pyrosetta_with_flags
 
 
 def trim_to_graft_start(pose, resnum):
@@ -104,42 +108,61 @@ def process_secondary_results(
     secondary_results_table.to_json(secondary_write_path)
 
 
-table_path = "/home/dzorine/phos_binding/pilot_runs/loop_grafting/fragment_tables/ploop_fragment_set/tables/1ang_3_contact_ploop_set_4_v3.json"
-dict_path = "/home/dzorine/phos_binding/pilot_runs/loop_grafting/fragment_tables/ploop_fragment_set/dicts/1ang_3_contact_ploop_set_4_v3.bin"
-
-pyrosetta.init()
-param_json = json.loads(sys.argv[1])
-name_hash = hash(param_json)
-param_dict = dict(param_json)
-
-params_for_helices = [
-    param_dict[i] for i in range(1, param_dict["num_helices"] + 1)
-]
-
-# Use the wrapper to create the MakeBundle Mover
-bundle_maker = helix_bundle_maker_wrapper(
-    param_dict["helix_length"], *params_for_helices, degrees=True
+@click.command()
+@click.option("-o", "--outdir")
+@click.option("-p", "--param-json")
+@click.option(
+    "-f",
+    "--flags-file",
+    default="/home/dzorine/phos_binding/run_files/p_ligand_quiet.flags",
 )
+def main(out_dir, param_json, flags_file):
 
-# Empty pose to helical bundle!
-pose = pyrosetta.rosetta.core.pose.Pose()
-bundle_maker.apply(pose)
-inf = pyrosetta.rosetta.core.pose.PDBInfo(pose)
-inf.name(f"param_bundle_{name_hash}.pdb")
-pose.pdb_info(inf)
+    table_path = "/home/dzorine/phos_binding/pilot_runs/loop_grafting/fragment_tables/ploop_fragment_set/tables/1ang_3_contact_ploop_set_4_v3.json"
+    dict_path = "/home/dzorine/phos_binding/pilot_runs/loop_grafting/fragment_tables/ploop_fragment_set/dicts/1ang_3_contact_ploop_set_4_v3.bin"
 
-if len(pose.residues):
-    results = scan_for_n_term_helical_grafts(pose, table_path, dict_path)
-    results["allowed_res"] = results.apply(
-        lambda x: [*range(1, len(pose.residues) + 1)], axis=1
+    run_pyrosetta_with_flags(flags_file)
+    param_json = json.loads(param_json)
+    name_hash = hash(param_json)
+    param_dict = dict(param_json)
+
+    params_for_helices = [
+        param_dict[i] for i in range(1, param_dict["num_helices"] + 1)
+    ]
+
+    # Use the wrapper to create the MakeBundle Mover
+    bundle_maker = helix_bundle_maker_wrapper(
+        param_dict["helix_length"], *params_for_helices, degrees=True
     )
-    results = results.rename(
-        index=str, columns={"frag_feature_to_start": "loop_func_to_bb_start"}
-    )
-    sec_results = scan_for_inv_rot(pose, results, table_path, dict_path)
-    if sec_results:
-        # do the grafting
-        process_secondary_results(sec_results, pose)
-        # dump the pdb and params
-        # update the DataFrame
-        # dump the dataframe
+
+    # Empty pose to helical bundle!
+    pose = pyrosetta.rosetta.core.pose.Pose()
+    bundle_maker.apply(pose)
+    inf = pyrosetta.rosetta.core.pose.PDBInfo(pose)
+    inf.name(f"param_bundle_{name_hash}.pdb")
+    pose.pdb_info(inf)
+
+    if len(pose.residues):
+        results = scan_for_n_term_helical_grafts(pose, table_path, dict_path)
+        results["allowed_res"] = results.apply(
+            lambda x: [*range(1, len(pose.residues) + 1)], axis=1
+        )
+        results = results.rename(
+            index=str,
+            columns={"frag_feature_to_start": "loop_func_to_bb_start"},
+        )
+        rot_table_path = "/home/dzorine/phos_binding/pilot_runs/loop_grafting/fragment_tables/inverse_rotamer/tables/inverse_ptr_exchi7_rotamers_1ang_v1.json"
+        rot_dict_path = "/home/dzorine/phos_binding/pilot_runs/loop_grafting/fragment_tables/inverse_rotamer/dicts/inverse_ptr_exchi7_rotamers_1ang_v1.bin"
+        sec_results = scan_for_inv_rot(
+            pose, results, rot_table_path, rot_dict_path
+        )
+        if sec_results:
+            # do the grafting
+            process_secondary_results(sec_results, pose, out_dir)
+            # dump the pdb and params
+            # update the DataFrame
+            # dump the dataframe
+
+
+if __name__ == "__main__":
+    main()
