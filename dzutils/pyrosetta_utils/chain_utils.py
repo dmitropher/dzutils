a15db992e449e21abb57ed2f6839bb5a8a7cf837
dzorine <dzorine@ibex.dhcp.baker>
2019-08-14 18:22:22 -0700

    afternoon commit

--- dzutils/pyrosetta_utils/chain_utils.py
+++ dzutils/pyrosetta_utils/chain_utils.py
@@ -26,6 +26,8 @@ def pose_from_chain(pose, chain):
     """
     return pose.split_by_chain()[chain]
 
+def chain_of(pose,resnum):
+    return [num for num in range(1,pose.num_chains() + 1) if resnum <= pose.chain_end(num) and resnum >= pose.chain_begin(num) ][0]
 
 def pose_excluding_chain(pose, *chain_nums):
     """
