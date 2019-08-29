d060b885215931dd976cb77939fe4d7566c3e84d
dzorine <dzorine@ibex.local>
2019-08-29 14:13:45 -0700

    added some tools to make benchmarking rotamer tables easier

--- dzutils/pyrosetta_utils/phos_binding/misc_scripts/inverse_rotamer_benchmarking.py
+++ dzutils/pyrosetta_utils/phos_binding/misc_scripts/inverse_rotamer_benchmarking.py
@@ -1,7 +1,12 @@
+from itertools import product, groupby
+
 import pyrosetta
 
 import numpy as np
 import pandas as pd
+import getpy as gp
+import scipy.spatial
+from scipy.spatial.kdtree import KDTree
 
 from xbin import XformBinner as xb
 
@@ -14,7 +19,9 @@ from dzutils.pyrosetta_utils.phos_binding import (
     phospho_residue_inverse_rotamer_rts,
 )
 
-type_dict = {}
+type_dict = {
+    "PTR": ("TYR", pyrosetta.rosetta.core.chemical.VariantType.PHOSPHORYLATION)
+}
 
 
 def get_residue_type_from_name(resname):
@@ -29,7 +36,7 @@ def get_residue_type_from_name(resname):
 
 def get_rotamer_residue_from_name(resname):
     residue_type = get_residue_type_from_name(resname)
-    return pyrosetta.rosetta.core.conformation.Residue(residue_type)
+    return pyrosetta.rosetta.core.conformation.Residue(residue_type, False)
 
 
 def get_rotamer_pose(resname, *chis):
@@ -42,38 +49,53 @@ def get_rotamer_pose(resname, *chis):
     return rotamer_pose
 
 
-def value_to_pose(value, data_table,conversion_type,*data_table_fields,residue_type_name=None):
+def value_to_pose(
+    value,
+    data_table,
+    conversion_type,
+    *data_table_fields,
+    residue_type_name=None,
+):
     if conversion_type == "from_file":
-        return pyrosetta.pose_from_file( data_table[value][*data_table_fields])
+        return pyrosetta.pose_from_file(
+            data_table[data_table["index"] == value][[*data_table_fields]]
+        )
     if conversion_type == "from_chis":
-        #get chis from the data table somehow
-        chis = get_chis(value,data_table)
-        return get_rotamer_pose(residue_type_name,*chis)
-
+        # get chis from the data table somehow
+        chis = data_table[data_table["index"] == value][[*data_table_fields]]
+        return get_rotamer_pose(residue_type_name, *chis)
 
 
 def rotamer_rmsd(pose_1, pose_2, super=True):
     if super:
         return pyrosetta.rosetta.core.scoring.all_atom_rmsd(pose_1, pose_2)
     else:
-        return pyrosetta.rosetta.core.scoring.all_atom_rmsd_nosuper(pose_1, pose_2)
+        return pyrosetta.rosetta.core.scoring.all_atom_rmsd_nosuper(
+            pose_1, pose_2
+        )
 
 
-def process_hits_by_rmsd(hits_df, **kwargs):
+def process_hits_by_rmsd(
+    hits_df, data_table, conversion_type, *data_table_fields, super=True
+):
     """
     Get the rmsd for the hits and attach it to the hits df
 
     default is to use the super full atom rmsd
     """
     ideal_poses = [
-        value_to_pose(value, **kwargs) for value in hits_df["value"]
+        value_to_pose(
+            value,
+            data_table,
+            conversion_type,
+            *data_table_fields,
+            residue_type_name=name,
+        )
+        for value, name in zip(hits_df["value"], hits_df["res_type_name"])
     ]
     pose_pairs = zip(hits_df["rot_pose"].copy(), ideal_poses)
-    hits_df.loc["rmsd_to_ideal"] = pd.Series(
-        [
-            rotamer_rmsd(*pair, kwargs["super"] if "super" in kwargs else True)
-            for pair in pose_pairs
-        ]
+    hits_df["rmsd_to_ideal"] = pd.Series(
+        [rotamer_rmsd(*pair, super=super) for pair in pose_pairs]
     )
 
     rmsd_to_ideal_df = hits_df
@@ -81,12 +103,49 @@ def process_hits_by_rmsd(hits_df, **kwargs):
     return rmsd_to_ideal_df
 
 
-def process_misses(misses_df, **kwargs):
-    return
+def kd_tree_from_rotamer_table(chi_table, *fields):
+    chis = list(chi_table[[*fields]])
+    chis.sort()
+    unique_chis = np.array(list(k for k, _ in groupby(chis)))
+    return KDTree(unique_chis), unique_chis
+
+
+def process_misses(misses_df, data_table, *rotamer_fields):
+    nearest_chi_tree, chi_data = kd_tree_from_rotamer_table(
+        data_table, *rotamer_fields
+    )
+    columns = [
+        f"chi_{i}"
+        for i in range(1, len(misses_df.columns) + 1)
+        if f"chi_{i}" in misses_df
+    ]
+    miss_chis = zip(
+        misses_df["res_type_name"], *[misses_df[col] for col in columns]
+    )
+    miss_rmsd = [
+        rotamer_rmsd(
+            pose,
+            get_rotamer_pose(
+                restype_chis[0],
+                *chi_data[nearest_chi_tree.query(restype_chis[1:])[1]],
+            ),
+        )
+        for pose, restype_chis in zip(data_table["rot_pose"], miss_chis)
+    ]
+    misses_df["rmsd_to_ideal"] = pd.Series(miss_rmsd)
+    return misses_df
 
 
 def test_hash_table(
-    table, test_data, xbin_cart_resl=1, xbin_ori_resl=15, **kwargs
+    table,
+    test_data,
+    xbin_cart_resl=1,
+    xbin_ori_resl=15,
+    data_table=None,
+    data_table_path="",
+    data_table_index_conversion="from_chis",
+    data_table_rotamer_fields=("chis",),
+    super_hits_before_rmsd=True,
 ):
     """
     Try the test_data rotamers on the table, return hits and misses with info
@@ -100,7 +159,7 @@ def test_hash_table(
     misses should have rmsd to the closest "ideal" query, or if not provided
     just the chis and the key
     """
-    res_type_names,rot_poses, rts, chi_tuples = zip(
+    res_type_names, rot_poses, rts, chi_tuples = zip(
         *[
             (
                 rot_info[0],
@@ -122,18 +181,54 @@ def test_hash_table(
     miss_mask = hits_mask == False
     all_results_df = pd.DataFrame(
         {
-            **{"res_type_name":res_type_names},
-            **{"rot_pose": rot_poses},
-            **{"rt": rts},
+            **{
+                "res_type_name": res_type_names,
+                "rot_pose": rot_poses,
+                "rt": rts,
+                "key": test_keys,
+            },
             **{f"chi_{i}": chis for i, chis in enumerate(zip(*chi_tuples), 1)},
-            **{"key": test_keys},
         }
     )
     hits_df = all_results_df[hits_mask].copy()
-    hits_keys = np.array(hits_df["key"].copy())
-    hits_df.loc["value"] = table[hits_keys]
+    hits_keys = hits_df["key"].copy()
+    if data_table is None:
+        pd.read_json(data_table_path)
+    hits_df["value"] = pd.Series(table[np.array(hits_keys)])
     misses_df = all_results_df[miss_mask].copy()
-    hits = process_hits_by_rmsd(hits_df, **kwargs)
-    misses = process_misses(misses_df, **kwargs)
+
+    hits = process_hits_by_rmsd(
+        hits_df,
+        data_table,
+        data_table_index_conversion,
+        *data_table_rotamer_fields,
+        super=super_hits_before_rmsd,
+    )
+    misses = process_misses(misses_df, data_table, *data_table_rotamer_fields)
 
     return hits, misses
+
+
+def build_test_rotamers_from_resname(residue_type_name, margin, granularity):
+    res_type = get_residue_type_from_name(residue_type_name)
+    rots = [
+        tuple([rot.chi(i) for i in range(1, rot.nchi() + 1)])
+        for rot in pyrosetta.rosetta.core.pack.rotamer_set.bb_independent_rotamers(
+            res_type
+        )
+    ]
+    return [
+        (residue_type_name, chi1, chi2, chi3)
+        for start_chi1, start_chi2, start_chi3 in rots
+        for chi1, chi2, chi3 in product(
+            np.linspace(start_chi1 - margin, start_chi1 + margin, granularity),
+            np.linspace(start_chi2 - margin, start_chi2 + margin, granularity),
+            np.linspace(start_chi3 - margin, start_chi3 + margin, granularity),
+        )
+    ]
+
+
+"/home/dzorine/projects/phos_binding/pilot_runs/loop_grafting/fragment_tables/inverse_rotamer/dicts/benchmarking_1_inv_rot_0.125_ang_2.5_deg.bin"
+"/home/dzorine/projects/phos_binding/pilot_runs/loop_grafting/fragment_tables/inverse_rotamer/dicts/benchmarking_1_inv_rot_0.25_ang_5.0_deg.bin"
+"/home/dzorine/projects/phos_binding/pilot_runs/loop_grafting/fragment_tables/inverse_rotamer/dicts/benchmarking_1_inv_rot_0.5_ang_10.0_deg.bin"
+"/home/dzorine/projects/phos_binding/pilot_runs/loop_grafting/fragment_tables/inverse_rotamer/dicts/benchmarking_1_inv_rot_1.0_ang_15.0_deg.bin"
